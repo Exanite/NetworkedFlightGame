449ca2ef95634ca9f0110b37e653edf88e4a4fc9
Exanite <exanite78012@gmail.com>
2021-10-09 02:29:33 -0400

    Add EventBus

---
+++ NetworkedFlightGame/Assets/Source/EventBus.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Source
+{
+    public class EventBus : MonoBehaviour
+    {
+        private Dictionary<Type, List<object>> listenerLists;
+
+        private void Awake()
+        {
+            listenerLists = new Dictionary<Type, List<object>>();
+        }
+
+        public void AddListener<T>(IEventListener<T> listener) where T : Event
+        {
+            var type = typeof(T);
+
+            if (!listenerLists.ContainsKey(typeof(T)))
+            {
+                listenerLists.Add(type, new List<object>());
+            }
+
+            listenerLists[type].Add(listener);
+        }
+
+        public void RemoveListener<T>(IEventListener<T> listener) where T : Event
+        {
+            if (!listenerLists.TryGetValue(typeof(T), out var listenerList))
+            {
+                return;
+            }
+
+            listenerList.Remove(listener);
+        }
+
+        public void PushEvent<T>(T e) where T : Event
+        {
+            var type = typeof(T);
+
+            if (!listenerLists.TryGetValue(type, out var listenerList))
+            {
+                return;
+            }
+
+            foreach (var listener in listenerList)
+            {
+                ((IEventListener<T>)listener).On(e);
+            }
+        }
+    }
+
+    public interface IEventListener<in T> where T : Event
+    {
+        void On<T>(T e);
+    }
+
+    public abstract class Event { }
+}
