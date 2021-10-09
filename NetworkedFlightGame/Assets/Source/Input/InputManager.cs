621a8a8a4dfefd31f2ec9e7a7fce9c0367094e3d
randompast <matteroflight@gmail.com>
2021-10-09 19:58:20 -0400

    split ship into reticle and ship scripts with Exanite's InputDistributor guidance/script

--- NetworkedFlightGame/Assets/Source/Input/InputManager.cs
+++ NetworkedFlightGame/Assets/Source/Input/InputManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Source.Input
@@ -6,13 +8,19 @@ namespace Source.Input
     public class InputManager : MonoBehaviour
     {
         public Ship ship;
+        public Reticle reticle;
         
         private InputActions actions;
 
         private void Awake()
         {
             actions = new InputActions();
-            actions.Player.SetCallbacks(ship);
+            // actions.Player.SetCallbacks(ship);
+            actions.Player.SetCallbacks(
+                new InputDistributor(
+                    new List<InputActions.IPlayerActions>() { 
+                        ship, reticle
+                }));
         }
 
         private void OnEnable()
