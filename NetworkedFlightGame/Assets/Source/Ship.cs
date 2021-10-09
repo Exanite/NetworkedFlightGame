cc5616613dfabb20d701a68c7c1c23c8e3c463a0
randompast <matteroflight@gmail.com>
2021-10-09 18:24:42 -0400

    added highlighting to keypressed UI reticle overlay

--- NetworkedFlightGame/Assets/Source/Ship.cs
+++ NetworkedFlightGame/Assets/Source/Ship.cs
@@ -15,8 +15,11 @@ public class Ship : MonoBehaviour, InputActions.IPlayerActions
     [Range(1f, 100f)]
     public float thrust;
 
-    Rigidbody rb;
+    private Rigidbody rb;
+
     public GameObject reticle;
+    private Dictionary<string, float> keysPressed = new Dictionary<string, float>();
+    float reticleAlpha = 0.117f;
 
     float cursorLockTime = 0;
     private void Awake(){
@@ -25,9 +28,18 @@ public class Ship : MonoBehaviour, InputActions.IPlayerActions
         rb.maxAngularVelocity = 2f;
     }
 
+    private void updateDict(InputAction.CallbackContext context){
+        var s = context.control.displayName;
+        if(context.performed){ 
+            Debug.Log(s);
+            keysPressed[s] = 0.8f;
+        }
+    }
+
     public void OnMove(InputAction.CallbackContext context)
     {
         // W, A, S, D Keys
+        updateDict(context);
         var input = context.action.ReadValue<Vector2>();
         vflags.x = input.x;
         vflags.y = input.y;
@@ -36,6 +48,7 @@ public class Ship : MonoBehaviour, InputActions.IPlayerActions
     public void OnThrust(InputAction.CallbackContext context)
     {
         // R, F Keys
+        updateDict(context);
         var input = context.action.ReadValue<float>();
         vflags.z = -input;
     }
@@ -43,6 +56,7 @@ public class Ship : MonoBehaviour, InputActions.IPlayerActions
     public void OnRoll(InputAction.CallbackContext context)
     {
         // Q, E Keys
+        updateDict(context);
         var input = context.action.ReadValue<float>();
         qflags.z = -input;
     }
@@ -57,7 +71,9 @@ public class Ship : MonoBehaviour, InputActions.IPlayerActions
     public void OnFire(InputAction.CallbackContext context)
     {
         // Left Click
-        Debug.Log("Fire");
+        if(cursorLockTime > 2){ 
+            Debug.Log("Fire");
+        }
     }
 
     public void OnPointer(InputAction.CallbackContext context)
@@ -72,17 +88,18 @@ public class Ship : MonoBehaviour, InputActions.IPlayerActions
         // Debug.Log("toggle reticle");
         foreach (Transform child in reticle.transform)
         {
-            Color on  = new Color(1f,1f,1f,0.117f);
+            Color on  = new Color(1f,1f,1f,reticleAlpha);
             Color off = new Color(1f,1f,1f,0.0f);
-            // if(child.GetComponent<RawImage>()){
-            //     RawImage ri = child.GetComponent<RawImage>();
-            //     ri.color = ri.color.a < 0.1f ? on : off;
-            // }
+            if(child.GetComponent<RawImage>()){
+                RawImage ri = child.GetComponent<RawImage>();
+                ri.color = ri.color.a < 0.1f ? on : off;
+            }
             if(child.GetComponent<Text>()){
                 Text t = child.GetComponent<Text>();
-                t.color = t.color.a < 0.1f ? on : off;
+                t.color = t.color.a < 0.1f || t.name == "Y" ? on : off;
             }
         }
+        updateDict(context);
     }
 
     public void OnQUIT(InputAction.CallbackContext context)
@@ -113,9 +130,27 @@ public class Ship : MonoBehaviour, InputActions.IPlayerActions
         addTorque(4.0f, qflags.z, Vector3.forward); //roll
     }
 
+    public void keyHighlighter(){
+        foreach (Transform child in reticle.transform)
+        {
+            if(child.GetComponent<Text>()){
+                Text t = child.GetComponent<Text>();
+                string name = child.name.ToLower();
+                if (keysPressed.ContainsKey(name) && t.color.a >= 0.1f) {
+                    float a = Mathf.Max( keysPressed[name], reticleAlpha);
+                    t.color = new Color(1f,1f,1f,a);
+                    keysPressed[name] = keysPressed[name] > reticleAlpha
+                            ? keysPressed[name] - Time.deltaTime * 1f
+                            : reticleAlpha;
+                }
+            }
+        }
+    }
+
     public void Update(){
         if(Cursor.visible){ cursorLockTime += Time.deltaTime; }
         addImpulse();
         if(cursorLockTime > 2){ addTorques(); }
+        keyHighlighter();
     }
 }
