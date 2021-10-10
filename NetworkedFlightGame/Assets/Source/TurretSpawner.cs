f0e8072f30abd8726c55e533e5f890c15230e48f
randompast <matteroflight@gmail.com>
2021-10-10 16:13:41 -0400

    minor fixes, removed camera from prefab

--- NetworkedFlightGame/Assets/Source/TurretSpawner.cs
+++ NetworkedFlightGame/Assets/Source/TurretSpawner.cs
@@ -8,16 +8,18 @@ public class TurretSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ring(15f, 1.5f, 5);
+        ring(15f,  1.5f, 5);
+        ring(15f, -1.5f, 5);
     }
     void ring(float radius, float z, int n){
         Vector3 p = transform.position;
         for(int i = 0; i < n; i++){
-            // float a = Mathf.PI * 2 / i;
-            float a = i * 360f / n;
+            float a = i * Mathf.PI * 2 / n;
+            // float a = i * 360f / n;
             p.x = radius * Mathf.Sin(a);
             p.y = radius * Mathf.Cos(a);
             p.z = z;
+            // Quaternion rot = new Quaternion()
             GameObject turret = Instantiate(turretPrefab, p, transform.rotation);
             turret.transform.parent = transform;
         }
