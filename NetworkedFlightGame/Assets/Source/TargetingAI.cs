ce64752546f7a514c3b3fd20cbcd0e3e493769e2
Exanite <exanite78012@gmail.com>
2021-10-10 18:50:44 -0400

    Change turrets to always target the closest ship

--- NetworkedFlightGame/Assets/Source/TargetingAI.cs
+++ NetworkedFlightGame/Assets/Source/TargetingAI.cs
@@ -1,8 +1,8 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using Source.Client;
+using System.Linq;
 using Source;
+using Source.Client;
+using UnityEngine;
 
 public class TargetingAI : MonoBehaviour
 {
@@ -11,103 +11,139 @@ public class TargetingAI : MonoBehaviour
     private Vector3 initialRotation;
     public float maxAngle = 90f; //degrees
 
-    float rate = 2f;
-    float rechargeTime;
-    float dataRecordingRate = 0.1f;
-    float dataRechargeTime;
+    private readonly float rate = 2f;
+    private float rechargeTime;
+    private readonly float dataRecordingRate = 0.1f;
+    private float dataRechargeTime;
 
     private List<Vector3> list;
     private Dictionary<int, Ship> playersById;
     private Ship target;
 
     // public BulletManager bulletManager;
-    void Start(){
+    private void Start()
+    {
         rechargeTime = rate;
         dataRechargeTime = dataRecordingRate;
         initialRotation = transform.forward;
 
         list = new List<Vector3>();
-        for(int i = 0; i < 3; i++){
-            list.Add( new Vector3(0f,0f,0f) );
+        for (var i = 0; i < 3; i++)
+        {
+            list.Add(new Vector3(0f, 0f, 0f));
         }
         // playersById = transform.root.GetComponent<ClientPlayerManager>().playersById;
-        playersById = GameObject.Find("Client(Clone)").GetComponent<ClientPlayerManager>().playersById;
-        foreach (var ship in playersById){
-            target = ship.Value;
-        }
+
+        playersById = FindObjectOfType<ClientPlayerManager>().playersById;
     }
 
-    bool withinAngularThreshold(GameObject target){
+    private bool withinAngularThreshold(GameObject target)
+    {
         return true;
     }
 
-    Vector3 listV(List<Vector3> list){
+    private Vector3 listV(List<Vector3> list)
+    {
         // effectively decasteljau's algorithm
-        for(int i = 0; i < list.Count - 1; i++){
-            list[i] = list[i] - list[i+1];
+        for (var i = 0; i < list.Count - 1; i++)
+        {
+            list[i] = list[i] - list[i + 1];
         }
-        return (list[0] / dataRecordingRate);
+
+        return list[0] / dataRecordingRate;
     }
 
-    void Fire(){
+    private void Fire()
+    {
         // Use best aiming direction
-        Vector3 p = transform.position + transform.forward*10;
-        BeamProjectile bullet = Instantiate(bulletPrefab, p, transform.rotation);
+        var p = transform.position + transform.forward * 10;
+        var bullet = Instantiate(bulletPrefab, p, transform.rotation);
         bullet.transform.localScale *= 2f;
-        BeamProjectile beamscript = bullet.GetComponent<BeamProjectile>();
+        var beamscript = bullet.GetComponent<BeamProjectile>();
 
-        Rigidbody bulletRB = bullet.GetComponent<Rigidbody>();
+        var bulletRB = bullet.GetComponent<Rigidbody>();
         bulletRB.velocity = transform.forward.normalized * 300;
         // bullet.transform.parent = bulletManager.transform;
     }
 
-    void TryFire(){
+    private void TryFire()
+    {
         rechargeTime -= Time.deltaTime;
         // Vector3 pointing = Quaternion.Euler(transform.rotation);
         // float angle = Quaternion.Angle(transform.rotation, initialRotation);
-        float angle = Vector3.Angle(transform.forward, initialRotation);
+        var angle = Vector3.Angle(transform.forward, initialRotation);
 
         // Debug.Log(angle, maxAngle);
-        if(rechargeTime < 0 && angle < maxAngle){
+        if (rechargeTime < 0 && angle < maxAngle)
+        {
             // Debug.Log("Fire");
             rechargeTime = rate;
             Fire();
         }
     }
 
-    void updateAngles(){
+    private void updateAngles()
+    {
         // Naive, aim directly at target
-        Vector3 dp = target.transform.position - transform.position;
+        var dp = target.transform.position - transform.position;
         Debug.DrawRay(transform.position, dp, Color.green);
         // transform.rotation = Quaternion.LookRotation(dp.normalized, Vector3.up);
     }
 
-    void updateList(){
+    private void updateList()
+    {
         dataRechargeTime -= Time.deltaTime;
-        if(dataRechargeTime < 0){
-            for(int i = 0; i < list.Count - 1; i++){
-                list[i+1] = list[i];
+        if (dataRechargeTime < 0)
+        {
+            for (var i = 0; i < list.Count - 1; i++)
+            {
+                list[i + 1] = list[i];
             }
+
             list[0] = target.transform.position;
             dataRechargeTime = dataRecordingRate;
         }
     }
 
-    void listLook(){
-        Vector3 v = listV(new List<Vector3>(list));
-        Vector3 dp = (target.transform.position - transform.position);
+    private void listLook()
+    {
+        var v = listV(new List<Vector3>(list));
+        var dp = target.transform.position - transform.position;
         Debug.DrawRay(transform.position + dp, v, Color.red);
         Debug.DrawRay(transform.position, dp + v, Color.white);
         //Aim where object is going
-        Quaternion aimDir = Quaternion.LookRotation((dp+v).normalized, Vector3.up);
+        var aimDir = Quaternion.LookRotation((dp + v).normalized, Vector3.up);
         // transform.rotation = Quaternion.RotateTowards(transform.rotation, aimDir, maxAngle);
         transform.rotation = aimDir;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (target != null){
+        var targetRange = 500f;
+        var closestDistance = Mathf.Infinity;
+        var closestShip = playersById.Values.First();
+
+        foreach (var ship in playersById.Values)
+        {
+            var distance = Vector3.Distance(transform.position, ship.transform.position);
+
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestShip = ship;
+            }
+        }
+
+        target = closestShip;
+
+        if (closestDistance > targetRange)
+        {
+            target = null;
+        }
+
+        if (target != null)
+        {
             updateList();
             updateAngles();
             listLook();
