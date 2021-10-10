c26d19dedd9960d20a2f392927ee0e7453f01b3b
Exanite <exanite78012@gmail.com>
2021-10-10 15:13:28 -0400

    Add ProjectileRegistry.GetId(projectilePrefab)

--- NetworkedFlightGame/Assets/Source/ProjectileRegistry.cs
+++ NetworkedFlightGame/Assets/Source/ProjectileRegistry.cs
@@ -11,8 +11,8 @@ namespace Source
         public bool TryGet(int prefabId, out Beam projectilePrefab)
         {
             projectilePrefab = null;
-            
-            if (prefabId < 0 ||prefabId >= projectilePrefabs.Count)
+
+            if (prefabId < 0 || prefabId >= projectilePrefabs.Count)
             {
                 return false;
             }
@@ -20,6 +20,14 @@ namespace Source
             projectilePrefab = projectilePrefabs[prefabId];
 
             return true;
+        }
+
+        /// <summary>
+        /// Use if you want to reference projectiles by prefab on the Ship/turrets and then get the id from here
+        /// </summary>
+        public int GetId(Beam projectilePrefab)
+        {
+            return projectilePrefabs.IndexOf(projectilePrefab);
         }
     }
 }
